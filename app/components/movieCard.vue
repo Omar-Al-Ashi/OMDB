78d37ac8e520a4b6b20f3a15a94e15f95b064af0
Omar <omar.ashi@hotmail.com>
2020-09-09 14:00:54 +0400

    Add movieCard component

---
+++ app/components/movieCard.vue
@@ -0,0 +1,44 @@
+<template lang="html">
+    <FlexboxLayout backgroundColor="#faebd7" height="200" width="100%" @tap="movieCardClicked(id)">
+        <Image :src="picture" height="100" width="100" backgroundColor="aqua"/>
+
+        <FlexboxLayout flexDirection="column" justifyContent="space-around">
+            <Label :text="title" class="text"/>
+            <Label :text="score" class="text"/>
+        </FlexboxLayout>
+    </FlexboxLayout>
+</template>
+
+<script>
+    export default {
+        props: {
+            id: {
+                type: String
+            },
+            picture: {
+                type: String
+            },
+            title: {
+                type: String
+            },
+            score: {
+                type: Number
+            }
+        },
+        data() {
+            return {}
+        },
+        methods: {
+            movieCardClicked(id) {
+                this.$emit('clicked', id);
+            }
+        }
+    }
+</script>
+
+<style lang="css" scoped>
+    .text {
+        font-size: 20;
+        color: #0F336D;
+    }
+</style>
